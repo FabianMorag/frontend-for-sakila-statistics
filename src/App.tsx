76b255b33258d3e100c72f9808f797ff362b9f46
Fabián Morag <fabian.morag@innovex.cl>
2026-02-17 11:20:03 -0300

    refactor: replace SVG icon with a span for settings button in the sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -39,21 +39,7 @@ export default function App() {
                 className="is-drawer-close:tooltip-right is-drawer-close:tooltip"
                 data-tip="Settings"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  fill="none"
-                  stroke="currentColor"
-                  className="inline-block my-1.5 size-4"
-                >
-                  <path d="M20 7h-9"></path>
-                  <path d="M14 17H5"></path>
-                  <circle cx="17" cy="17" r="3"></circle>
-                  <circle cx="7" cy="7" r="3"></circle>
-                </svg>
+                <span className="icon-[codicon--settings]" />
                 <span className="is-drawer-close:hidden">Settings</span>
               </button>
             </li>
