96ae3970991381d1b3b32602f6f7ee1977a5c668
Fabián Morag <fabian.morag@innovex.cl>
2026-03-11 11:19:11 -0300

    feat: add zustand store for global loading state management

---
+++ src/stores/useGlobalLoading.ts
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+
+export const useGlobalLoading = create(
+  combine({ globalLoading: false }, (set) => ({
+    setGlobalLoading: (loading: boolean) => set({ globalLoading: loading }),
+  })),
+);
