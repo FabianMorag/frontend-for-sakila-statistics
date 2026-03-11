42b37e66d8e8018d186c5e010fad3315934ce05f
Fabián Morag <fabian.morag@innovex.cl>
2026-03-11 11:54:58 -0300

    feat: implement useGlobalFetch hook for data fetching with global loading state management

---
+++ src/hooks/useGlobalFetch.ts
@@ -0,0 +1,14 @@
+import useSWR from 'swr'
+import { useGlobalLoading } from '@/stores/useGlobalLoading'
+import { useEffect } from 'react'
+
+export const useGlobalFetch = <T>(
+  url: string,
+  fn: (url: string) => Promise<T>
+) => {
+  const { data, error, isLoading } = useSWR(url, fn)
+  const setGlobalLoading = useGlobalLoading(state => state.setGlobalLoading)
+  useEffect(() => setGlobalLoading(isLoading), [isLoading])
+
+  return { data, error }
+}
