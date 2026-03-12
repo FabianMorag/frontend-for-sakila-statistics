eae78e55849f8357d4c4d4c330053a68c505acd3
Fabián Morag <fabian.morag@innovex.cl>
2026-03-12 16:15:27 -0300

    feat: update FilmDetails component with enhanced background and icon representation

--- src/routes/films/FilmDetails.tsx
+++ src/routes/films/FilmDetails.tsx
@@ -34,21 +34,8 @@ export default function FilmDetails() {
 
       <div className="bg-base-200 shadow-xl card">
         <figure className="px-6 pt-6">
-          <div className="flex justify-center items-center bg-gradient-to-br from-primary/20 to-secondary/20 rounded-xl w-full h-48">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="opacity-30 w-20 h-20"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1.5}
-                d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
-              />
-            </svg>
+          <div className="flex justify-center items-center bg-linear-to-br from-primary/30 to-secondary/30 rounded-xl w-full h-48">
+            <span className="text-4xl icon-[meteor-icons--film]" />
           </div>
         </figure>
 
