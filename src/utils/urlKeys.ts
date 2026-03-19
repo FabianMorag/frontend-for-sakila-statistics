db156cf445fd21d2f759512486dab894f185bdcc
Fabián Morag <fabian.morag@innovex.cl>
2026-03-19 11:13:18 -0300

    feat: add customer-related URL keys for fetching customer data

--- src/utils/urlKeys.ts
+++ src/utils/urlKeys.ts
@@ -2,3 +2,6 @@ export const getFilmsKey = (page: number) => `/films?limit=6&page=${page}`
 export const getFilmDetailsKey = (id: string) => `/films/${id}`
 export const getTopCategoriesKey = () => `/reports/top-categories`
 export const getRevenueOverTimeKey = () => `/reports/revenue-over-time`
+export const getCustomerListKey = () => `/customers`
+export const getTopCustomerKey = () => `/customers/top-customers`
+export const getCustomerKey = (id: string) => `/customers/${id}`
