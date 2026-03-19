bfcb8e005cfec034a87f562068e6305f0c43a354
Fabián Morag <fabian.morag@innovex.cl>
2026-03-19 11:17:20 -0300

    feat: move TopCategory and RevenueOverTime types to a separate file for better organization

--- src/services/reports.ts
+++ src/services/reports.ts
@@ -1,14 +1,5 @@
 import { fetcher } from '@/lib/fetcher'
-
-type TopCategory = {
-  name: string
-  total_revenue: number
-}
-
-type RevenueOverTime = {
-  date: string
-  total_revenue: string
-}
+import type { RevenueOverTime, TopCategory } from '@/types/reports'
 
 export const getTopCategories = (url: string) => fetcher<TopCategory[]>(url)
 export const getRevenueOverTime = (url: string) =>
---
+++ src/types/reports.ts
@@ -0,0 +1,9 @@
+export type TopCategory = {
+  name: string
+  total_revenue: number
+}
+
+export type RevenueOverTime = {
+  date: string
+  total_revenue: string
+}
